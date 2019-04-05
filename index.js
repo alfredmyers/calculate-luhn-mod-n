611ac0f4994b3185ea6cf27903971beef333f368
Alfred Myers <git@alfredmyers.com>
2019-04-05 15:17:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+/* 
+    Code based on https://en.wikipedia.org/wiki/Luhn_mod_N_algorithm
+    with minimal modifications.
+    The main difference being "input" is an array of codePoints
+    i.e. Mapping characters to code-points was done elsewhere, if at all.
+*/
+
+module.exports = function calculateLuhnModN(input, n) {
+	var factor = 2;
+	var sum = 0;
+
+	// Starting from the right and working leftwards is easier since 
+	// the initial "factor" will always be "2" 
+	for (var i = input.Length - 1; i >= 0; i--) {
+		var codePoint = input[i];
+		var addend = factor * codePoint;
+
+		// Alternate the "factor" that each "codePoint" is multiplied by
+		factor = (factor == 2) ? 1 : 2;
+
+		// Sum the digits of the "addend" as expressed in base "n"
+		addend = Math.floor(addend / n) + (addend % n);
+		sum += addend;
+	}
+
+	// Calculate the number that must be added to the "sum" 
+	// to make it divisible by "n"
+	var remainder = sum % n;
+	var checkCodePoint = (n - remainder) % n;
+
+	return checkCodePoint;
+}
