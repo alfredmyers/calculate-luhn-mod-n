46a64623e85bf777e5f80ad5ccd005c6cece037d
Alfred Myers <git@alfredmyers.com>
2019-04-12 14:14:52 +0000

    Added EditorConfig and formated source code

--- index.js
+++ index.js
@@ -8,27 +8,27 @@
 */
 
 module.exports = function calculateLuhnModN(input, n) {
-	var factor = 2;
-	var sum = 0;
+  var factor = 2;
+  var sum = 0;
 
-	// Starting from the right and working leftwards is easier since 
-	// the initial "factor" will always be "2" 
-	for (var i = input.length - 1; i >= 0; i--) {
-		var codePoint = input[i];
-		var addend = factor * codePoint;
+  // Starting from the right and working leftwards is easier since 
+  // the initial "factor" will always be "2" 
+  for (var i = input.length - 1; i >= 0; i--) {
+    var codePoint = input[i];
+    var addend = factor * codePoint;
 
-		// Alternate the "factor" that each "codePoint" is multiplied by
-		factor = (factor == 2) ? 1 : 2;
+    // Alternate the "factor" that each "codePoint" is multiplied by
+    factor = (factor == 2) ? 1 : 2;
 
-		// Sum the digits of the "addend" as expressed in base "n"
-		addend = Math.floor(addend / n) + (addend % n);
-		sum += addend;
-	}
+    // Sum the digits of the "addend" as expressed in base "n"
+    addend = Math.floor(addend / n) + (addend % n);
+    sum += addend;
+  }
 
-	// Calculate the number that must be added to the "sum" 
-	// to make it divisible by "n"
-	var remainder = sum % n;
-	var checkCodePoint = (n - remainder) % n;
+  // Calculate the number that must be added to the "sum" 
+  // to make it divisible by "n"
+  var remainder = sum % n;
+  var checkCodePoint = (n - remainder) % n;
 
-	return checkCodePoint;
+  return checkCodePoint;
 }
--- index.test.js
+++ index.test.js
@@ -1,19 +1,19 @@
 const calculateLuhnModN = require('./index');
 
 const data = [
-    {
-        n: 36,
-        input: '1134806PJFB000010013CD18',
-        output: 'D'
-    },
-    //test based on algorithm description in https://en.wikipedia.org/wiki/Luhn_algorithm
-    {
-        n: 10,
-        input: '7992739871',
-        output: '3'
-    }
+  {
+    n: 36,
+    input: '1134806PJFB000010013CD18',
+    output: 'D'
+  },
+  //test based on algorithm description in https://en.wikipedia.org/wiki/Luhn_algorithm
+  {
+    n: 10,
+    input: '7992739871',
+    output: '3'
+  }
 ];
 
 data.forEach(item => {
-    test('', () => { expect(calculateLuhnModN([...item.input].map(c => Number.parseInt(c, item.n)), item.n)).toBe(Number.parseInt(item.output, item.n))});
+  test('', () => { expect(calculateLuhnModN([...item.input].map(c => Number.parseInt(c, item.n)), item.n)).toBe(Number.parseInt(item.output, item.n)) });
 });
