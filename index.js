054155fa709904496c2b99fd1ea45ab0041aa89c
Alfred Myers <git@alfredmyers.com>
2019-06-12 15:02:29 +0000

    Added JSDoc type annotations

--- index.js
+++ index.js
@@ -5,6 +5,38 @@
     with minimal modifications.
 */
 
+/**
+ * @module calculateLuhnModN
+ */
+
+/**
+ * A value to be mapped to and from a code point.
+ * @typedef {any} Value
+ */
+
+/**
+ * Maps a value to a code point.
+ * @callback valueToCodePoint
+ * @param {!Value} value - a value (typically a character) to be mapped to a number.
+ * @returns {number}
+ */
+
+/**
+ * Maps a code point to a value.
+ * @callback codePointToValue
+ * @param {!number} codePoint - numeric value of the code point.
+ * @returns {Value}
+ */
+
+/**
+ * Calculates Luhn Mod N control values
+ * @param {!valueToCodePoint} codePointFromCharacter - Function that maps a value (typically a character) to a code point.
+ * @param {!codePointToValue} characterFromCodePoint - Function that maps a code point to a value (typically a character).
+ * @param {!number} n - modulo.
+ * @param {!Value[]} input - String of values (typically characters) for which the control value will be calculated.
+ * @returns {Value} - The control value.
+ */
+
 module.exports = function calculateLuhnModN(codePointFromCharacter, characterFromCodePoint, n, input) {
   var factor = 2;
   var sum = 0;
